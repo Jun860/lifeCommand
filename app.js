c9279b06eccfeca0af1bfafe9a22e549e2328aa6
JUN <jiangjun860@iie.ac.cn>
2017-10-08 15:55:21 +0800

    add upload page

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var sql = require('./routes/sql');
 
 var app = express();
@@ -22,7 +21,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/sql', sql);
 
 // catch 404 and forward to error handler
