f1aca5d471e504e3428c0a5f8cb702f35dd688ba
JUN <jiangjun860@iie.ac.cn>
2017-10-18 19:28:02 +0800

    add  viz bar

---
+++ method/sql/key.js
@@ -0,0 +1,5 @@
+var not_in = `'旅游','医院'`;
+
+module.exports = {
+    not_in:not_in
+}
--- routes/sql.js
+++ routes/sql.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var sql_model = require('../model/sql.js').sql;
+var key = require('../method/sql/key.js');
 
 router.get('/insert', async function(req, res, next) {
   var sql = new sql_model();
@@ -21,7 +22,17 @@ router.get('/select_bytime', async function(req, res, next) {
   var sql = new sql_model();
   sql.getReq({
     key:`sum(money),extract(month from now()- interval '${temp_params.month} month') as month`,
-    condition:`where (extract(month from time) = (extract(month from now()- interval '${temp_params.month} month'))) and (extract(year from time) = extract(year from now()- interval '${temp_params.month} month')) and action not in ('旅游','医院')`
+    condition:`where (extract(month from time) = (extract(month from now()- interval '${temp_params.month} month'))) and (extract(year from time) = extract(year from now()- interval '${temp_params.month} month')) and action not in (${key.not_in})`
+  });
+  res.json(await sql.select());
+});
+
+router.get('/select_bytime_bar', async function(req, res, next) {
+  var temp_params = req.query;
+  var sql = new sql_model();
+  sql.getReq({
+    key:`action,sum(money) as money`,
+    condition:`where (extract(month from time) = (extract(month from now()- interval '${temp_params.month} month'))) and (extract(year from time) = extract(year from now()- interval '${temp_params.month} month')) and action not in (${key.not_in}) group by action order by money DESC`
   });
   res.json(await sql.select());
 });
