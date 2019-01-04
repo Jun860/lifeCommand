51fe0166abd5d0d5b5ad5b0ba5a4376ede0762c7
JUN <jiangjun860@iie.ac.cn>
2019-01-04 13:49:21 +0800

    update event list

--- model/sql.js
+++ model/sql.js
@@ -15,31 +15,39 @@ sql.prototype = {
     select: async function (params) {
         var temp_params = {
             key: `to_char(time,'MM-DD') as time_day,action,things,money`,
-            condition: `where (extract(month from time) = (extract(month from now()- interval '${params.month} month'))) and (extract(year from time) = extract(year from now()- interval '${params.month} month')) ORDER BY time DESC limit ${params.num_pro_page} offset ${params.num_pro_page * (params.current_page - 1)}`
+            condition: `where (extract(month from time) = (extract(month from now()- interval '${params.month} month'))) and (extract(year from time) = extract(year from now()- interval '${params.month} month')) and "where" = '${params.cag}' ORDER BY time DESC limit ${params.num_pro_page} offset ${params.num_pro_page * (params.current_page - 1)}`
         };
         var res = await pgsql.select(`"BBZ"."Mrecord"`, temp_params.key, temp_params.condition);
         return res;
     },
-    select_bytime: async function (month) {
+    select_bytime: async function (month, cag) {
         var temp_params = {
             key: `sum(money),extract(year from now()- interval '${month} month') as year,extract(month from now()- interval '${month} month') as month`,
-            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and action not in (${key.not_in})`
+            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and action not in (${key.not_in}) and "where" = '${cag}'`
         }
         var res = await pgsql.select(`"BBZ"."Mrecord"`, temp_params.key, temp_params.condition);
         return res;
     },
-    select_bytime_all: async function (month) {
+    select_bytime_all: async function (month, cag) {
         var temp_params = {
             key: `sum(money),extract(year from now()- interval '${month} month') as year,extract(month from now()- interval '${month} month') as month`,
-            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month'))`
+            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and "where" = '${cag}'`
         }
         var res = await pgsql.select(`"BBZ"."Mrecord"`, temp_params.key, temp_params.condition);
         return res;
     },
-    select_bytime_bar: async function (month) {
+    select_bytime_bar: async function (month, cag) {
         var temp_params = {
             key: `(action||'-'||things) as action,sum(money) as money`,
-            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and action not in (${key.not_in}) group by action,things order by money DESC`
+            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and action not in (${key.not_in}) and "where" = '${cag}' group by action,things order by money DESC`
+        }
+        var res = await pgsql.select(`"BBZ"."Mrecord"`, temp_params.key, temp_params.condition);
+        return res;
+    },
+    get_event_list: async function (month) {
+        var temp_params = {
+            key: `"where",sum(money) as money`,
+            condition: `where (extract(month from time) = (extract(month from now()- interval '${month} month'))) and (extract(year from time) = extract(year from now()- interval '${month} month')) and action not in (${key.not_in}) group by "where" order by money DESC`
         }
         var res = await pgsql.select(`"BBZ"."Mrecord"`, temp_params.key, temp_params.condition);
         return res;
--- routes/sql.js
+++ routes/sql.js
@@ -2,37 +2,44 @@ var express = require('express');
 var router = express.Router();
 var sql_model = require('../model/sql.js').sql;
 
-router.get('/insert', async function(req, res, next) {
+router.get('/insert', async function (req, res, next) {
   var sql = new sql_model();
 
   res.json(await sql.insert(req.query));
 });
 
-router.get('/select', async function(req, res, next) {
+router.get('/select', async function (req, res, next) {
   var sql = new sql_model();
 
   res.json(await sql.select(req.query));
 });
 
-router.get('/select_bytime', async function(req, res, next) {
+router.get('/select_bytime', async function (req, res, next) {
   var temp_params = req.query;
   var sql = new sql_model();
 
-  res.json(await sql.select_bytime(temp_params.month));
+  res.json(await sql.select_bytime(temp_params.month, temp_params.cag));
 });
 
-router.get('/select_bytime_all', async function(req, res, next) {
+router.get('/select_bytime_all', async function (req, res, next) {
   var temp_params = req.query;
   var sql = new sql_model();
 
-  res.json(await sql.select_bytime_all(temp_params.month));
+  res.json(await sql.select_bytime_all(temp_params.month, temp_params.cag));
 });
 
-router.get('/select_bytime_bar', async function(req, res, next) {
+router.get('/select_bytime_bar', async function (req, res, next) {
   var temp_params = req.query;
   var sql = new sql_model();
 
-  res.json(await sql.select_bytime_bar(temp_params.month));
+  res.json(await sql.select_bytime_bar(temp_params.month, temp_params.cag));
+});
+
+router.get('/get_cag', async function (req, res, next) {
+  var temp_params = req.query;
+  var sql = new sql_model();
+
+  res.json(await sql.get_event_list(temp_params.month));
 });
 
 module.exports = router;
