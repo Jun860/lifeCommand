a248146d90d7fbbd6ff4ed762173ac8333f7564e
JUN <jiangjun860@iie.ac.cn>
2019-07-08 12:10:01 +0800

    add log out function

--- method/sql/pgsql.js
+++ method/sql/pgsql.js
@@ -1,6 +1,8 @@
 var pg = require('pg');
 var config = require('../../config/sql_config.js').sql_config;
 
+const Json2csvParser = require('json2csv').Parser;
+
 async function insert(database, params) {
     var pool = new pg.Pool(config);
     var res;
@@ -41,7 +43,33 @@ async function select(database, key, condition) {
     }
 }
 
+async function select_tocsv(database, key, condition) {
+    var pool = new pg.Pool(config);
+    var res;
+
+    const fields = ['time', 'action', 'things', 'money', 'who', 'where'];
+    const json2csvParser = new Json2csvParser({ fields });
+
+    try {
+        var client = await pool.connect();
+
+        var sql = `SELECT ${key} FROM ${database} ${condition}`;
+        console.log('select_sql', sql);
+
+        res = (await client.query(sql)).rows;
+        var csv = json2csvParser.parse(res);
+    } catch (err) {
+        console.error(err);
+        csv = "error"
+    } finally {
+        client.release();
+        return csv;
+    }
+}
+
+
 module.exports = {
     insert: insert,
-    select: select
+    select: select,
+    select_tocsv: select_tocsv
 };
